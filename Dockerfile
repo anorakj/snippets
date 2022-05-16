162726477193f42785e4857af25feafb534e61ac
anorakj <nicholasjcc@gmail.com>
2022-05-16 14:09:21 +0800

    Modify Pipeline

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ RUN python -m venv /opt/venv
 # Make sure we use the virtualenv:
 ENV PATH="/opt/venv/bin:$PATH"
 
-RUN pip install pydantic 
+RUN pip install pydantic
+RUN pip install rich
 
 FROM python:3.8.7-slim AS build-image
 COPY --from=compile-image /opt/venv /opt/venv
--- pipeline.py
+++ pipeline.py
@@ -7,28 +7,32 @@ import abc
 from abc import ABC
 from typing import List, Dict
 
+from rich import traceback
+
+traceback.install()
+
 
 class Pipeline(ABC):
     def __init__(self, **kwargs):
-        self.upstreams: List[Pipeline] = []
-        self.downstreams: List[Pipeline] = []
-        self.paramters = kwargs
-        self.finished = False
+        self.upstream: List[Pipeline] = []
+        self.downstream: List[Pipeline] = []
+        self.parameters: Dict = kwargs
+        self.finished: bool = False
 
     def __getattr__(self, item):
-        if item in self.paramters:
-            return self.paramters[item]
-        else:
+        try:
+            return self.parameters[item]
+        except KeyError:
             raise AttributeError
 
     def run(self):
-        for pipeline in self.upstreams:
+        for pipeline in self.upstream:
             if not pipeline.finished:
                 return
-        self.check_types()
+        self._check_parameter_types()
         self.run_job()
         self.finished = True
-        for pipeline in self.downstreams:
+        for pipeline in self.downstream:
             pipeline.run()
 
     @abc.abstractmethod
@@ -40,18 +44,21 @@ class Pipeline(ABC):
     def parameter_types(self) -> Dict[str, type]:
         pass
 
-    def check_types(self):
+    def _check_parameter_types(self):
         for parameter, parameter_type in self.parameter_types.items():
-            if parameter not in self.paramters:
+            if parameter not in self.parameters:
                 raise TypeError(f"{parameter} is not defined")
-            if not isinstance(self.paramters[parameter], parameter_type):
+            if not isinstance(self.parameters[parameter], parameter_type):
                 raise TypeError(f"{parameter} is not of type {parameter_type}")
 
+    def update_parameters(self, **kwargs):
+        self.parameters.update(kwargs)
+
     def add_downstream(self, pipeline: Pipeline):
-        if pipeline in self.downstreams:
+        if pipeline in self.downstream:
             return
-        self.downstreams.append(pipeline)
-        pipeline.upstreams.append(self)
+        self.downstream.append(pipeline)
+        pipeline.upstream.append(self)
 
     def add_serial_pipeline(self, pipelines: List[Pipeline]):
         node = self
@@ -59,8 +66,9 @@ class Pipeline(ABC):
             node.add_downstream(pipeline)
             node = pipeline
 
-    def transmit_paramters(self, pipeline: Pipeline):
-        pipeline.paramters.update(self.paramters)
+    def transmit_parameters(self):
+        for pipeline in self.downstream:
+            pipeline.parameters.update(self.parameters)
 
 
 class P1(Pipeline):
@@ -73,13 +81,23 @@ class P1(Pipeline):
 
     def run_job(self):
         print(self.a)
-        for downstream in self.downstreams:
-            self.transmit_paramters(downstream)
+        self.transmit_parameters()
 
 
 class P2(Pipeline):
     def run_job(self):
         print(self.a)
+        self.transmit_parameters()
+
+    @property
+    def parameter_types(self):
+        return {"a": int}
+
+
+class P3(Pipeline):
+    def run_job(self):
+        print(self.a)
+        raise Exception("test")
 
     @property
     def parameter_types(self):
@@ -89,5 +107,7 @@ class P2(Pipeline):
 if __name__ == "__main__":
     p = P1(a=1)
     p2 = P2()
+    p3 = P3()
     p.add_downstream(p2)
+    p2.add_downstream(p3)
     p.run()
